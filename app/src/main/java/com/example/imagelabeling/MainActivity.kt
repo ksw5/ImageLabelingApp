603fc8e615d4be68fc25269ad038a5c1b847b4ae
Kieran <kieranwindley@gmail.com>
2021-12-11 18:18:19 -0500

    initial commit

---
+++ app/src/main/java/com/example/imagelabeling/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.imagelabeling
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+
+
+class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            // launch camera app
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(imageBitmap)
+            //prepare bitmap for mlkit
+            val inputForMlkit = InputImage.fromBitmap(imageBitmap, 0)
+            //initialize image labeling api
+            val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+            labeler.process(inputForMlkit)
+                .addOnSuccessListener { labels ->
+                    // Task completed successfully
+                    Log.i("Kieran", "Successfully proccessed")
+                    var result = ""
+                    for (label in labels) {
+                        result = result + "\n" + label.text + " - " + label.confidence
+                        binding.textView.text = result
+
+                        Log.i("Kieran", result)
+                    }
+
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    Log.e("Kieran", "Error processing")
+                }
+
+
+
+        }
+
+    }
+
+
+}
